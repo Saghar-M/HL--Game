9593e0ddd448576dd76e39aa61c499a893a3a07a
Saghar-M <khairis24982@masseyhigh.school.nz>
2026-03-11 12:24:04 +1300

    HL not finished yet

--- B_01_HL_Game.py
+++ B_01_HL_Game.py
@@ -1,3 +1,6 @@
+import math
+
+
 # checks users enter yes (y) or no (n)
 
 def yes_no(question):
@@ -28,9 +31,11 @@ Roll the dice and try to win!
 
 
 
-# cheks for na integer more than 0 (allows <enter>)
+# cheks for na integer with optional upper /
+# lower limits and an optional exit code for infinite mode
+# quitting the game
 
-def int_check(question):
+def int_check(question, low=None, high=None, exit_code=None):
     while True:
 
         error = "please enter an integer that is 1 or more. "
@@ -55,6 +60,15 @@ def int_check(question):
              print(error)
 
 
+# calculate the maximum number of guesses
+def calc_guesses(low,high):
+    num_range = high - low + 1
+    max_raw =math.log2(num_range)
+    max_upped = math.ceil(max_raw)
+    max_guesses= max_upped + 1
+    return max_guesses
+
+
 # main Routine starts here
 
 # Initialise game variables
@@ -72,11 +86,17 @@ if want_instructions == "yes":
     instructions()
 
 # Ask user for number of rounds / infinite mode
-num_rounds = int_check("How many rounds would you like? push <enter> for infinite mode: ")
+num_rounds = int_check("Rounds <enter for infinite:",
+                       low=1,exit_code="")
 
 if num_rounds == "infinite":
     mode ="infinite"
     num_rounds = 5
+
+# Get Game parameters
+low_num = int_check("Low Number? ")
+high_num = int_check("High Number? ", low=low_num+1)
+guesses_allowed = calc_guesses(low_num, high_num)
 
 # Game loop starts here
 while rounds_played < num_rounds:
